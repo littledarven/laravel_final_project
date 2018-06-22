31669543699d16555f1ec1f04a5cccdcc802ddc6
Letícia Moreira <leticia.moreira@yandex.com>
2018-06-22 21:59:08 +0000

    CourseController updated

--- app/Http/Controllers/CourseController.php
+++ app/Http/Controllers/CourseController.php
@@ -11,10 +11,42 @@ class CourseController extends Controller
         $courses = Course::All();
         return view('courses/index',['courses'=>$courses]);
     }
-    public function create(CourseRequest $request)
+    public function create()
+    {
+        return view('courses/new');
+    }
+    public function store(CourseRequest $request)
     {
         $course = new Course;
         $course->name = $request->input('name');
         $course->total_time = $request->input('total_time');
+        if($course->save())
+        {
+            \Session::flash('status', 'Curso cadastrado com sucesso !');
+            return redirect('/courses');
+        } 
+        else 
+        {
+            \Session::flash('status', 'Ocorreu um erro ao cadastrar o curso !');
+            return view('courses/new');
+        }
+    }
+    public function edit($id)
+    {
+        $course = $Course::findOrFail($id);
+        return view('courses/edit',['course' => $course]);
+    }
+    public function update(StateRequest $request, $id)
+    {
+        $course = Course::findOrFail($id);
+        $course->name = $request->input('name');
+        $course->total_time = $request->input('total_time');
+    }
+    public function destroy($id)
+    {
+        $course = Course::findOrFail($id);
+        $course->delete();
+        \Session::flash('status', 'Curso deletado com sucesso.');
+        return redirect('/courses');
     }
 }
