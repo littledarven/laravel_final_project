33fb5a13d3e6e186b1da72e45c97d2a7cd63af83
Letícia Moreira <leticia.moreira@yandex.com>
2018-06-23 12:19:47 +0000

    Display problem fixed

--- resources/views/courses/index.blade.php
+++ resources/views/courses/index.blade.php
@@ -1,102 +1,53 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.app')
 
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header" style="text-align:center">Disciplinas Disponíveis</div>
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
-
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
-
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="https://fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Raleway:300,400,600" rel="stylesheet" type="text/css">
-
-    <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-</head>
-<body>
-    <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
-                </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
-
-                    </ul>
-
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
-                        <!-- Authentication Links -->
-                        @guest
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                            </li>
-                        @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }} <span class="caret"></span>
-                                </a>
-
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
-
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                        @endguest
-                    </ul>
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+                    <table class="table">
+                            <tr>
+                                <th>ID</th>
+                                <th>Nome</th>
+                                <th>Carga Horária</th>
+                                <th>Inscrever-me</th>
+                            </tr>
+                            @foreach($courses as $course)
+                            
+                            <tr>
+                                    <td>{{ $course->id }}</td>
+                                    <td>{{ $course->name }}</td>
+                                    <td>{{ $course->total_time}}</td>
+                                    <td>
+                                    <div id="buttons">
+                                        {{-- ARRUMAR !!!!!!--}}
+                                        {!! Form::open(['url' => "/enrollments/$course->id",'method' => 'post']) !!}
+                                        {!! Form::submit('+',['id' => 'enroll-button'])!!}
+                                        {!! Form::close() !!}
+                                    </div>
+                                    </td>
+                                </tr>
+                            @endforeach
+                            {{ $courses->links()}}
+                        </table>    
+                </main>
+            </div>
+        </body>
+        </html>
+        
+                    
                 </div>
             </div>
-        </nav>
-        <main class="py-4">
-                @yield('content')
-                <table class="table">
-                    <tr>
-                        <th>ID</th>
-                        <th>Nome</th>
-                        <th>Carga Horária</th>
-                        <th>Inscrever-me</th>
-                    </tr>
-                    @foreach($courses as $course)
-                    
-                    <tr>
-                            <td>{{ $course->id }}</td>
-                            <td>{{ $course->name }}</td>
-                            <td>{{ $course->total_time}}</td>
-                            <td>
-                            <div id="buttons">
-                                {!! Form::open(['url' => "/enrollments/$course->id",'method' => 'post']) !!}
-                                {!! Form::submit('+',['id' => 'enroll-button'])!!}
-                                {!! Form::close() !!}
-                            </div>
-                            </td>
-                        </tr>
-                    @endforeach
-                    {{ $courses->links()}}
-                </table>    
-        </main>
+        </div>
     </div>
-</body>
-</html>
+</div>
+@endsection
+                
