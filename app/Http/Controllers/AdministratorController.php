5b246292eae9fd1c4a9e5af13721df59a62c8e37
Letícia Moreira <leticia.moreira@yandex.com>
2018-06-24 17:05:38 +0000

    Admin verification and authorizatzion added

--- app/Http/Controllers/AdministratorController.php
+++ app/Http/Controllers/AdministratorController.php
@@ -3,8 +3,24 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Administrator;
+use App\User;
 class AdministratorController extends Controller
 {
-    //
+    public function index()
+    {
+        $admins = User::where('is_admin',1)->paginate(15);
+        return view('admins/index',['admins' => $admins]);
+    }
+    public function update($id)
+    {
+    	if((auth()->user()->is_admin==1))
+    	{
+	        $course = User::findOrFail($id);
+	        $course->is_admin = 1;
+	        $course->save();
+    	}
+        \Session::flash('status', 'Você não tem permissão para acessar essa área !');
+        return redirect('/admins');
+    }
 }
