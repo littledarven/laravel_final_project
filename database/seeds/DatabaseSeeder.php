5c32d60893f3ae664b2b37be98793828034fee39
Letícia Moreira <leticia.moreira@yandex.com>
2018-06-28 10:35:19 +0000

    Function created to create a master admin if there are no users existent

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\User;
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,6 +12,17 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        if(User::count()==0)
+        {
+        	$user = new User;
+        	$user->name = "Master";
+        	$user->is_admin = true;
+        	$user->email = "";
+        	$user->password = Hash::make("123");
+
+        	$user->save();
+
+
+        }
     }
 }
