ca30c6b3627ca2def0fe2c181db5da5c45d4004c
Letícia Moreira <leticia.moreira@yandex.com>
2018-06-28 10:33:14 +0000

    HasMany and BelongsToMany commands added to Course and User (student) models

--- app/Course.php
+++ app/Course.php
@@ -9,4 +9,9 @@ class Course extends Model
     protected $fillable = [
         'name', 'total_time',
     ];
+
+    public function students()
+    {
+        return $this->belongsToMany('App\User');
+    }
 }
