dacf770514c67f316d8fe91860a3be09c2cf10f4
Letícia Moreira <leticia.moreira@yandex.com>
2018-07-03 06:10:42 +0000

    Route.web updated to handle different views and the noficiation method

--- routes/web.php
+++ routes/web.php
@@ -15,8 +15,15 @@ Route::get('/', function () {
     return view('welcome');
 });
 Auth::routes();
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/home', 'HomeController@index')->name('home');			
 Route::get('enrollments/activate_enrollments','EnrollmentController@showInactivate');
+Route::get('enrollments/enrollment','EnrollmentController@studentIndex');
+Route::get('courses/allcourses','CourseController@allCourses');
+Route::get('/notifications/{id}','NotificationController@markAsRead');
+Route::get('/notifications2/{id}','NotificationController@markAsRead2');
+
+
+Route::get('/notifications','NotificationController@index');
 Route::resource('students','StudentController');
 Route::resource('enrollments','EnrollmentController');
 Route::resource('courses','CourseController');
