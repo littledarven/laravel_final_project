d5c89eed1567ee57778d04d92f37dbeaf4ed66fd
Letícia Moreira <leticia.moreira@yandex.com>
2018-07-03 08:52:56 +0000

    Controllers updated

--- app/Http/Controllers/EnrollmentController.php
+++ app/Http/Controllers/EnrollmentController.php
@@ -33,63 +33,76 @@ class EnrollmentController extends Controller
     {
         $students = User::find((auth()->user()->id));
         return view('enrollments/enrollment',['students'=> $students]);
-}
-public function store(EnrollmentRequest $request)
-{
- $enrollment = new Enrollment;
- $enrollment->student_id = auth()->user()->id;
- $course_id = $request->input('id');
- $enrollment->course_id = $request->input('id');
- $course = Course::findOrFail($course_id);
- if($enrollment->save())
- {
-    $course = Course::findOrFail($course_id); 
-    $course->max_students-=1;
-    $course->save();
+    }
+    public function store(EnrollmentRequest $request)
+    {
+       $enrollment = new Enrollment;
+       $enrollment->student_id = auth()->user()->id;
+       $course_id = $request->input('id');
+       $enrollment->course_id = $request->input('id');
+       $course = Course::findOrFail($course_id);
+       if($enrollment->save())
+       {
+            $course = Course::findOrFail($course_id); 
+            $course->max_students-=1;
+            $course->save();
             // auth()->user()->notify(new PendingAuthorisation ($course->name));
-    $admins = User::where('is_admin',1)->get();
-    foreach ($admins as $admin) 
-    {
-        $admin->notify(new PendingAuthorisation($course->name));
+            $admins = User::where('is_admin',1)->get();
+            foreach ($admins as $admin) 
+            {
+                $admin->notify(new PendingAuthorisation($course->name));
+            }   
+            \Session::flash('status', 'Matrícula em estado de aprovação !');
+            return redirect('/enrollments');
+        }
+        else
+        {
+        \Session::flash('status', 'Falha ao efetuar a matrícula !');
+        return redirect('/courses');
     }
-    \Session::flash('status', 'Matrícula em estado de aprovação !');
-    return redirect('/enrollments');
 }
-else
-{
-   \Session::flash('status', 'Falha ao efetuar a matrícula !');
-   return redirect('/courses');
-}
+    public function showInactivate()
+    {
+        if(auth()->user()->is_admin==0)
+        {
+            \Session::flash('status', 'Você não tem permissão para acessar essa área !');
+            return redirect('/enrollments');
+        }
+        $users = User::where('is_admin','0');
+        $enrollments = Enrollment::select('student_id');
+        $students = User::whereIn('id', $enrollments)->paginate(15);
+        return view ('enrollments/activate_enrollments',['students' => $students]);
+    }
+    public function update($id)
+    {
+        if(auth()->user()->is_admin==0)
+        {
+            \Session::flash('status', 'Você não tem permissão para acessar essa área !');
+            return redirect('/enrollments');
+        }
 
-}
-public function showInactivate()
-{
-    if(auth()->user()->is_admin==0)
-    {
-        \Session::flash('status', 'Você não tem permissão para acessar essa área !');
-        return redirect('/enrollments');
+        $enrollment = Enrollment::findOrFail($id);
+        $enrollment->is_authorised = true;
+        $enrollment->save();
+        $student = User::find($enrollment->student_id);
+        $course = Course::find($enrollment->course_id);
+        $student->notify(new EnrollmentAuthorisationGranted($course->name));
+        $user = auth()->user();
+        \Session::flash('status', 'Matrícula aprovada !');
+        return redirect('/enrollments/activate_enrollments');
     }
-    $users = User::where('is_admin','0');
-    $enrollments = Enrollment::select('student_id');
-    $students = User::whereIn('id', $enrollments)->paginate(15);
-    return view ('enrollments/activate_enrollments',['students' => $students]);
-}
-public function update($id)
-{
-    if(auth()->user()->is_admin==0)
+    public function destroy($id)
     {
+        if((auth()->user()->is_admin==1))
+        {
+            $enroll = Enrollment::findOrFail($id);
+            $enroll->delete();
+            \Session::flash('status', 'Matrícula deletado com sucesso.');
+            return redirect('/courses');
+        }
         \Session::flash('status', 'Você não tem permissão para acessar essa área !');
-        return redirect('/enrollments');
-    }
+        return redirect('/courses');
+        
 
-    $enrollment = Enrollment::findOrFail($id);
-    $enrollment->is_authorised = true;
-    $enrollment->save();
-    $student = User::find($enrollment->student_id);
-    $course = Course::find($enrollment->course_id);
-    $student->notify(new EnrollmentAuthorisationGranted($course->name));
-    $user = auth()->user();
-    \Session::flash('status', 'Matrícula aprovada !');
-    return redirect('/enrollments/activate_enrollments');
-}
+    }
 }
