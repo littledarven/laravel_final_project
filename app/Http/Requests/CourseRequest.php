135d2b3c97a26dd664d31be39837a4c7ddf0a357
Letícia Moreira <leticia.moreira@yandex.com>
2018-07-03 06:09:13 +0000

    NotificationController created to handle ReadNotifications and the other ones were updated

--- app/Http/Requests/CourseRequest.php
+++ app/Http/Requests/CourseRequest.php
@@ -24,7 +24,26 @@ class CourseRequest extends FormRequest
     public function rules()
     {
         return [
-            //
+            'name' => 'string|required|min:5|max:30',
+            'total_time' => 'numeric|required|max:3',
+            'description' => 'min:10|max:300',
+            'max_students'=> 'numeric|max:3|min:1',
         ];
+    }
+    public function messages()
+    {
+        return [
+            'name.string' => 'O nome só pode conter letras !',
+            'name.max' => 'Minimo 5 máximo 30 caracteres !',
+            'name.min' => 'Minimo 5 máximo 30 caracteres !',
+            'total_time.numeric' => 'Somente números permitidos !',
+            'total_time.max' => 'Máximo de 3 números !',
+            'description.min' => 'Minimo 10 máximo 300 caracteres !'
+            'description.max' => 'Minimo 10 máximo 300 caracteres !'
+            'max_students.numeric' => 'Somente números permitidos !',
+            'max_students.max' => 'Minimo 1 máximo 3 caracteres',
+            'max_students.min' => 'Minimo 1 máximo 3 caracteres',
+
+        ]
     }
 }
